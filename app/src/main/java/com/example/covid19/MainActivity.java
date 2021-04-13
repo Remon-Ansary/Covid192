d1ed5cde858ffad6b2990bf5cb82c80b405da0ff
Remon Ansary <remonansary@gmail.com>
2021-04-13 20:46:08 +0600

    first commit

---
+++ app/src/main/java/com/example/covid19/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button call_button;
+    private Button button2;
+    private Button button;
+    private ImageView imageView7;
+    private ImageView imageView6;
+    private ImageView imageView5;
+    private ImageView imageView;
+    private TextView textView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private Spinner spinner;
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
